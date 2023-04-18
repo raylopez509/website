abf30bb534588296453ec4b8d8d67c3d1193056c
raylopez509 <raylopez509@gmail.com>
2023-04-17 23:51:47 -0700

    Added Tic Tac Toe and Project Page
    Added Tictactoe page and converted from plain javascript to React. Project Page has list of projects that I made.

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -10,12 +10,12 @@ export default function NavigationBar() {
             </div>
             <div className={styles.navbar}>
                 <Link href="/">Home</Link>
-                <Link href="/posts/first-post">Applications</Link>
+                <Link href="/projects">Projects</Link>
                 <Link href="/tictactoe">Tic Tac Toe</Link>
                 <Link href="/firstperson">First Person Selector</Link>
                 <Link href="/teamrandomizer">Team Randomizer</Link>
                 <Link href="/top10boardgames">Top 10 Board Games</Link>
-                <a href="#">Board Game Collection</a>
+                {/* <a href="#">Board Game Collection</a> */}
             </div>
         </>
     );
