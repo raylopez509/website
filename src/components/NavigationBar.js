9da00b19bec8273f2431687dd00e0005d1f9df4a
raylopez509 <raylopez509@gmail.com>
2024-08-12 21:28:36 -0700

    added quote marks, added classes for animation css, and changed tab size

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -3,54 +3,56 @@ import Link from "next/link";
 
 const links = [
   {
-    href: '/',
-    text: 'Home',
+    href: "/",
+    text: "Home",
   },
   {
-    href: '/projects',
-    text: 'Projects',
+    href: "/projects",
+    text: "Projects",
   },
   {
-    href: '/tictactoe',
-    text: 'Tic Tac Toe',
+    href: "/tictactoe",
+    text: "Tic Tac Toe",
   },
   {
-    href: '/firstperson',
-    text: 'First Person Selector',
+    href: "/firstperson",
+    text: "First Person Selector",
   },
   {
-    href: '/teamrandomizer',
-    text: 'Team Randomizer',
+    href: "/teamrandomizer",
+    text: "Team Randomizer",
   },
   {
-    href: 'top10boardgames',
-    text: 'Top 10 Board Games',
+    href: "top10boardgames",
+    text: "Top 10 Board Games",
   },
   // {
   //   href: '#',
   //   text: 'Board Game Collection',
   // },
-]
+];
 
 export default function NavigationBar() {
-    return (
-        <>
-            <div className={styles.header}>
-                <h1>Ray is on the Web</h1>
-                <p>i leik board games</p>
-            </div>
-            <div className={styles.navbar}>
-              {links.map(link => (
-                  <Link href={link.href}>{link.text}</Link>
-              ))}
-                {/* <Link href="/">Home</Link>
+  return (
+    <>
+      <div className={styles.header}>
+        <div class={styles.wrapper}>
+          <div class={styles.typingdemo}>Ray is on the web.</div>
+        </div>
+        <p>i leik board games</p>
+      </div>
+      <div className={styles.navbar}>
+        {links.map((link) => (
+          <Link href={link.href}>{link.text}</Link>
+        ))}
+        {/* <Link href="/">Home</Link>
                 <Link href="/projects">Projects</Link>
                 <Link href="/tictactoe">Tic Tac Toe</Link>
                 <Link href="/firstperson">First Person Selector</Link>
                 <Link href="/teamrandomizer">Team Randomizer</Link>
                 <Link href="/top10boardgames">Top 10 Board Games</Link> */}
-                {/* <a href="#">Board Game Collection</a> */}
-            </div>
-        </>
-    );
+        {/* <a href="#">Board Game Collection</a> */}
+      </div>
+    </>
+  );
 }
