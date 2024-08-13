fe5dab516e5d4cf62c6a0e8a06e1d6b1d9a9c3ca
raylopez509 <raylopez509@gmail.com>
2024-08-12 21:30:11 -0700

    added animation testing page

---
+++ src/pages/animation.js
@@ -0,0 +1,11 @@
+import styles from "../styles/animation.module.css";
+
+export default function Animation() {
+  return (
+    <>
+      <div class={styles.slideintext}>
+        <div class={styles.typingdemo}>What up.</div>
+      </div>
+    </>
+  );
+}
