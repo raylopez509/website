ffd29aed5ada2a915a754f185ccb05b79bdb323d
raylopez509 <raylopez509@gmail.com>
2024-08-12 21:27:41 -0700

    changed tab size from 4 to 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,24 +6,24 @@ import Link from "next/link";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-    return (
-        <>
-            <div className={styles.row}>
-                <div className={styles.side}>
-                    idk what's gonna go here, maybe a picture?
-                </div>
-                <Image
-                    src="/images/silhouette.jpg"
-                    height={500}
-                    width={500}
-                    alt="My Picture"
-                />
-                <div className={styles.main}>this is my website</div>
-            </div>
-            {/* Move footer to _app.js, just like navigation */}
-            <div className={styles.footer}>
-                <h2>All rights reserved by Ray®</h2>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className={styles.row}>
+        <div className={styles.side}>
+          idk what's gonna go here, maybe a picture?
+        </div>
+        <Image
+          src="/images/silhouette.jpg"
+          height={500}
+          width={500}
+          alt="My Picture"
+        />
+        <div className={styles.main}>this is my website</div>
+      </div>
+      {/* Move footer to _app.js, just like navigation */}
+      <div className={styles.footer}>
+        <h2>All rights reserved by Ray®</h2>
+      </div>
+    </>
+  );
 }
